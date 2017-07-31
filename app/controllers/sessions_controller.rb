6d50e33fb78934f9ed14a8cdc8b17feaf7e6a245
totycos <dv94@hotmail.fr>
2017-07-31 17:28:10 +0200

    implemente advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,14 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+=begin Pareil que :
+    if params[:session][:remember_me] == '1'
+    remember(user)
+    else
+    forget(user)
+    end
+=end
       redirect_to user
     else
 flash.now[:danger] = "Invalid email/password combination" #.now = le flash disparait la page suivante
@@ -14,7 +22,7 @@ flash.now[:danger] = "Invalid email/password combination" #.now = le flash dispa
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,16 +4,40 @@ module SessionsHelper
     session[:user_id] = user.id #envoi un cookie temporaire pour garder connecté l'utilisateur, et est détruit lorsqu'il quitte
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id #genere cookie pour l'id
+    cookies.permanent[:remember_token] = user.remember_token #genere cookie pour le token
   end
+
+  # Returns the user corresponding to the remember token cookie.
+ def current_user
+     if (user_id = session[:user_id]) #verifie si l'utilisateur s'est connecté
+       @current_user ||= User.find_by(id: user_id)
+     elsif (user_id = cookies.signed[:user_id]) #vérifie si l'utilisateur a des cookies permettant l'authentification
+       user = User.find_by(id: user_id) #compare les cookies à la BDD
+       if user && user.authenticated?(cookies[:remember_token]) #si l'utilisateur existe en que l'authentification avec le token crypté de notre BDD
+         log_in user
+         @current_user = user
+       end
+     end
+   end
 
   def logged_in?
    !current_user.nil?
  end
 
+ # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
  # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
